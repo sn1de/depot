8070b19b0f9ff842fc71d4447685345c627ad721
Brad Schneider <bschneider@containerstore.com>
2014-01-20 21:41:48 -0600

    Tutorial section 11.5

--- db/migrate/20131203061055_price_line_items.rb
+++ db/migrate/20131203061055_price_line_items.rb
@@ -1,29 +1,16 @@
 class PriceLineItems < ActiveRecord::Migration
-  #def up
-  #  LineItem.all.each do |l|
-  #    l.price = Product.find_by(id: l.product_id).price
-  #    l.save!
-  #  end
-  #end
-  #
-  #def down
-  #  LineItem.all.each do |l|
-  #    l.price = nil
-  #    l.save
-  #  end
-  #end
-  def self.up
-    add_column :line_items, :price, :decimal,
-
-    say_with_time "Updating prices..." do
-      LineItem.find(:all).each do |lineitem|
-        lineitem.update_attribute :price, lineitem.product.price
-      end
-    end
+  def up
+   LineItem.all.each do |l|
+     l.price = Product.find_by(id: l.product_id).price
+     l.save!
+   end
   end
-
-  def self.down
-    remove_column :line_items, :price
+  
+  def down
+   LineItem.all.each do |l|
+     l.price = nil
+     l.save
+   end
   end
-
+ 
 end
