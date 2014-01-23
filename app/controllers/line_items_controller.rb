e0b5c197bdd5c3702200c0dd6afc137129b76004
Brad Schneider <bschneider@containerstore.com>
2014-01-22 21:50:34 -0600

    Ch 11 exercise 2 work in progress. Changed to decrement controller method.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -57,9 +57,25 @@ class LineItemsController < ApplicationController
     end
   end
 
+  def decrement
+    logger.debug "++ line item decrement ++"
+    line_item = LineItem.find(params[:id])
+    if line_item.quantity == 1
+      line_item.destroy
+    else
+      line_item.quantity -= 1
+      line_item.save
+    end
+
+    respond_to do |format|
+      format.html { redirect_to cart_url(line_item.cart_id), notice: "Quantity decremented."}
+    end
+  end
+
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    logger.debug "-- line item destroy --"
     if @line_item.quantity == 1
       @line_item.destroy
     else
