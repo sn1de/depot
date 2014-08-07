735b19a093da1a12ab1ba2509f586ab80e941239
Brad Schneider <bschneider@containerstore.com>
2014-08-07 12:33:46 -0500

    Completed ch12 ex 1 and 2.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,11 +64,14 @@ class ProductsController < ApplicationController
   def who_bought
     @product = Product.find(params[:id])
     @latest_order = @product.orders.order(:updated_at).last
-    if stale?(@latest_order)
+#   if stale?(@latest_order)
       respond_to do |format|
+        format.xml { render :xml => @product.to_xml(:include => :orders) }
         format.atom
+        format.html
+        format.json
       end
-    end
+#    end
   end
 
   private
